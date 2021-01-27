9a61d11229ef96d33ab1c74525d534b5b95b750c
Hau Dang <54978813+HauDang@users.noreply.github.com>
2021-01-27 12:36:24 +0100

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -101,12 +101,8 @@ class ModelSetup(model.ContactMechanicsBiot):
         """ Set arguments for mesh size (as explained in other tutorials)
         and the name fo the export folder.
         """
-        # super().__init__(mesh_args, folder_name)
-        # params kemur nú með folder_name
         super().__init__(params)
  
-        # Set additional case specific fields
-        # For now I take this out but can be used to set paremeters later on (see main.py (THM))
         tips, frac_pts, frac_edges = fracture_infor(fracture)
         self.box = box
         self.tips = tips
@@ -114,19 +110,7 @@ class ModelSetup(model.ContactMechanicsBiot):
         self.frac_edges = frac_edges
         self.mesh_args = mesh_args
         
-       
-        # Fix names used for the variables:
-        # Names for mechanics variables are set in a super class
-        self.scalar_variable = "p"
-        self.mortar_scalar_variable = "mortar_" + self.scalar_variable
-        
-        self.scalar_coupling_term = "robin_" + self.scalar_variable
-        self.scalar_parameter_key = "flow"
-
-        self.subtract_fracture_pressure = True
-
         # Scaling coefficients
-        # The use of this is not mature. Be careful!
         self.scalar_scale = 1 #1e5
         self.length_scale = 1 #100
         self.scalar_source_value = 1
@@ -137,9 +121,6 @@ class ModelSetup(model.ContactMechanicsBiot):
         self.end_time = self.time_step * 10
         self.step = 0
 
-        self.initial_aperture = 1e-3 #/ self.length_scale
-        # Dirichlet boundary condition for pressure
-        self.s_0 = 1 * pp.MEGA / self.scalar_scale
         # solution 
         self.disp = []
         self.pres = []
@@ -147,22 +128,16 @@ class ModelSetup(model.ContactMechanicsBiot):
     def before_newton_loop(self):
         self.convergence_status = False
         self._iteration = 0
-        self.set_parameters()
-    def set_parameters(self):
-        self.set_scalar_parameters()
-        self.set_mechanics_parameters()
-    def prepare_simulation(self):
-
-        
+        self._set_parameters()
+    def prepare_simulation(self):        
         self.create_grid()
         self.set_rock_and_fluid()
-        self.set_parameters()
+        self._set_parameters()
         self._assign_variables()
         self._assign_discretizations()
-        self.initial_condition()
+        self._initial_condition()
         self._discretize()
         self._initialize_linear_solver()
-
         g_max = self._nd_grid()
         self.viz = pp.Exporter(
             g_max, file_name="mechanics", folder_name=self.viz_folder_name
@@ -199,17 +174,17 @@ class ModelSetup(model.ContactMechanicsBiot):
         p = g.nodes; self.p = p[[0,1],:].T
         self.t = g.cell_nodes().indices.reshape((3, g.num_cells), order='f').T
         return gb
-    def source_mechanics(self, g):
+    def _source_mechanics(self, g):
         return np.zeros(g.num_cells * self._Nd)
 
-    def source_scalar(self, g):
+    def _source_scalar(self, g):
         if g.dim == self._Nd:
-            values = np.zeros(g.num_cells) + self.time_step*self.step
+            values = np.zeros(g.num_cells)
         else:
             values = self.scalar_source_value * np.ones(g.num_cells)
         return values
 
-    def bc_type_mechanics(self, g):
+    def _bc_type_mechanics(self, g):
         all_bf, east, west, north, south, top, bottom = self._domain_boundary_sides(g)
         bc = pp.BoundaryConditionVectorial(g, north + south, "dir")
         # Default internal BC is Neumann. We change to Dirichlet for the contact
@@ -221,7 +196,7 @@ class ModelSetup(model.ContactMechanicsBiot):
         
         return bc
 
-    def bc_type_scalar(self, g):
+    def _bc_type_scalar(self, g):
         all_bf, east, west, north, south, top, bottom = self._domain_boundary_sides(g)
         bc = pp.BoundaryCondition(g)
         # Define boundary condition on faces
@@ -229,140 +204,22 @@ class ModelSetup(model.ContactMechanicsBiot):
         bc.is_neu[east] = True   
         return bc # displacement
 
-    def bc_values_mechanics(self, g):
+    def _bc_values_mechanics(self, g):
         # Set the boundary values
         all_bf, east, west, north, south, top, bottom = self._domain_boundary_sides(g)
         values = np.zeros((g.dim, g.num_faces))
 
         values[0, south] = 0
         values[1, south] = 0
-        values[0, north] = 1e6 #self.time_step
-        values[1, north] = 0
+        values[0, north] = 0
+        values[1, north] = -1e6*np.sin(self.step*0.5)
         return values.ravel("F")
-    def bc_values_scalar(self, g):
+    def _bc_values_scalar(self, g):
         all_bf, east, west, north, south, top, bottom = self._domain_boundary_sides(g)
         bc_values = np.zeros(g.num_faces)
         # bc_values[east] = 0
         # bc_values[west] = 0
         return bc_values
-    
-    def set_mechanics_parameters(self):
-        
-        gb = self.gb
-        for g, d in gb:
-            if g.dim == self.Nd:
-                # Rock parameters
-                rock = self.rock
-                lam = rock.LAMBDA * np.ones(g.num_cells) / self.scalar_scale
-                mu = rock.MU * np.ones(g.num_cells) / self.scalar_scale
-                C = pp.FourthOrderTensor(mu, lam)
-
-                # Define boundary condition
-                bc = self.bc_type_mechanics(g)
-                # BC and source values
-                bc_val = self.bc_values_mechanics(g)
-                source_val = self.source_mechanics(g)
-
-                pp.initialize_data(
-                    g,
-                    d,
-                    self.mechanics_parameter_key,
-                    {
-                        "bc": bc,
-                        "bc_values": bc_val,
-                        "source": source_val,
-                        "fourth_order_tensor": C,
-                        "time_step": self.time_step,
-                        "biot_alpha": self._biot_alpha(g),
-                    },
-                )
-
-            elif g.dim == self._Nd - 1:
-                friction = self._set_friction_coefficient(g)
-                pp.initialize_data(
-                    g,
-                    d,
-                    self.mechanics_parameter_key,
-                    {"friction_coefficient": friction, "time_step": self.time_step},
-                )
-
-        for _, d in gb.edges():
-            mg: pp.MortarGrid = d["mortar_grid"]
-            pp.initialize_data(mg, d, self.mechanics_parameter_key)
-    def set_scalar_parameters(self):
-        tensor_scale = self.scalar_scale / self.length_scale ** 2
-        kappa = 1 * tensor_scale
-        mass_weight = 1 * self.scalar_scale
-        for g, d in self.gb:
-            bc = self.bc_type_scalar(g)
-            bc_values = self.bc_values_scalar(g)
-            source_values = self.source_scalar(g)
-
-            specific_volume = self._specific_volume(g)
-            diffusivity = pp.SecondOrderTensor(
-                kappa * specific_volume * np.ones(g.num_cells)
-            )
-
-            alpha = self.biot_alpha()
-            pp.initialize_data(
-                g,
-                d,
-                self.scalar_parameter_key,
-                {
-                    "bc": bc,
-                    "bc_values": bc_values,
-                    "mass_weight": mass_weight * specific_volume,
-                    "biot_alpha": alpha,
-                    "source": source_values,
-                    "second_order_tensor": diffusivity,
-                    "time_step": self.time_step,
-                },
-            )
-
-        # Assign diffusivity in the normal direction of the fractures.
-        for e, data_edge in self.gb.edges():
-            g_l, g_h = self.gb.nodes_of_edge(e)
-            mg = data_edge["mortar_grid"]
-            a_l = self._aperture(g_l)
-            # Take trace of and then project specific volumes from g_h
-            v_h = (
-                mg.primary_to_mortar_avg()
-                * np.abs(g_h.cell_faces)
-                * self._specific_volume(g_h)
-            )
-            # Division by a/2 may be thought of as taking the gradient in the normal
-            # direction of the fracture.
-            normal_diffusivity = kappa * 2 / (mg.secondary_to_mortar_avg() * a_l)
-            # The interface flux is to match fluxes across faces of g_h,
-            # and therefore need to be weighted by the corresponding
-            # specific volumes
-            normal_diffusivity *= v_h
-            data_edge = pp.initialize_data(
-                e,
-                data_edge,
-                self.scalar_parameter_key,
-                {"normal_diffusivity": normal_diffusivity},
-            )
-    def initial_condition(self, time=0):
-        """
-        Initial guess for Newton iteration, scalar variable and bc_values (for time
-        discretization).
-        """
-        super()._initial_condition()
-
-        for g, d in self.gb:
-            # Initial value for the scalar variable.
-            initial_scalar_value = np.zeros(g.num_cells)
-            d[pp.STATE].update({self.scalar_variable: initial_scalar_value})
-            if g.dim == self._Nd:
-                bc_values = self.bc_values_mechanics(g)
-                mech_dict = {"bc_values": bc_values}
-                d[pp.STATE].update({self.mechanics_parameter_key: mech_dict})
-
-        for _, d in self.gb.edges():
-            mg = d["mortar_grid"]
-            initial_value = np.zeros(mg.num_cells)
-            d[pp.STATE][self.mortar_scalar_variable] = initial_value
     def after_newton_convergence(self, solution, errors, iteration_counter):
         self.step = self.step+1
         self.assembler.distribute_variable(solution)
@@ -407,33 +264,10 @@ setup = ModelSetup(box, fracture, mesh_args, params)
 
 pp.run_time_dependent_model(setup, params)
 
-for i in range(3):
+for i in range(7):
     dispi = setup.disp[i]
     presi = setup.pres[i]
     presnod =  NN_recovery( presi, setup.p, setup.t)
     
     trisurf(setup.p, setup.t, face = None, index = None, value = presnod)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
